d5ee57105072c442031efc2f61d9b4601ee19905
aermakova <aleksandra.ermakova@valamis.com>
2023-11-23 12:26:54 +0200

    add: en locale, tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,scss, js}"], // what files will interapt with a project
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,44 @@
-const path = require('path'); // path - пакет ноды для работы с путями
+const path = require("path"); // path - пакет ноды для работы с путями
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    optimization: {
-        minimize: false
-    },
-    entry: './sourse/js/index.js', // точка входа, так же может быть объект с точками входа
-    mode: process.env.NODE_ENV || 'development', // можем из консоли попасть в исходник, в продакшене не можем
-    output: { // то, что нам выдаст вебпак
-        path: path.resolve(__dirname, 'dist'), // путь (папка дист)
-        filename: 'bundle.js', // название файла
-    },
-    module: {
-        rules: [
-            // когда вебпак видит что у нас есть файл с расширением hbs,
-            // он запускает лоадер
-            { test: /\.hbs$/, loader: "handlebars-loader" },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    "css-loader",
-                    "sass-loader",
-                ],
-            },
+  optimization: {
+    minimize: false,
+  },
+  entry: "./sourse/js/index.js", // точка входа, так же может быть объект с точками входа
+  mode: process.env.NODE_ENV || "development", // можем из консоли попасть в исходник, в продакшене не можем
+  output: {
+    // то, что нам выдаст вебпак
+    path: path.resolve(__dirname, "dist"), // путь (папка дист)
+    filename: "bundle.js", // название файла
+  },
+  module: {
+    rules: [
+      // когда вебпак видит что у нас есть файл с расширением hbs,
+      // он запускает лоадер
+      { test: /\.hbs$/, loader: "handlebars-loader" },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
         ],
-
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './sourse/index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: "main.css",
-        }),
-    ]
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./sourse/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "main.css",
+    }),
+  ],
 };
